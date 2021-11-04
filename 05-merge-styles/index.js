3bf51c4d0e9010c9a06e46c358b390752cd6582b
excluz1v <bmd4work@gmail.com>
2021-11-04 18:56:43 +0600

    feat: add solution to task #5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,40 @@
+const { readdir, rm, appendFile } = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+
+
+const stylesFolderPath = path.join(__dirname, 'styles');
+const stylesResultPath = path.join(__dirname, 'project-dist');
+
+async function readFolder(folderPath) {
+    let allFiles = await readdir(folderPath, { withFileTypes: true });
+    return allFiles;
+}
+
+async function createCssBundle(inputFolderPath, outputFolder) {
+    //delete bundle.css if exist
+    const cssBundlePath = path.join(outputFolder, 'bundle.css');
+    await rm(cssBundlePath, { force: true, recursive: true });
+
+    //read files in folder
+    let files = await readFolder(inputFolderPath);
+
+    //filter only css
+    let cssFiles = onlyCssFilter(files);
+    console.log(cssFiles);
+    for (let i = 0; i < cssFiles.length; i++) {
+        //create stream and write it to new bundle.css
+        const filePath = path.join(inputFolderPath, cssFiles[i].name);
+        const myReadebleStream = fs.createReadStream(filePath, 'utf-8');
+
+        myReadebleStream.on('data', async (chunk) => await appendFile(cssBundlePath, chunk));
+    }
+}
+
+function onlyCssFilter(allFiles) {
+    const cssFiles = allFiles.filter(file => (path.extname(file.name) === '.css' && file.isFile()));
+    return cssFiles;
+}
+
+
+createCssBundle(stylesFolderPath, stylesResultPath);
